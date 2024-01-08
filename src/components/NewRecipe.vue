aa67bd52cfae1cd8d7304a02523306d2e7e2f206
Chizo77 <chizo1coldpark3@gmail.com>
2024-01-08 14:47:09 +0800

    little fix

--- src/components/NewRecipe.vue
+++ src/components/NewRecipe.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="new-recipe-form">
-    <h2>Create New Recipe</h2>
+    <h2 id="title">Create New Recipe</h2>
     <form @submit.prevent="saveNewRecipe">
       <label for="name">Name:</label>
       <input v-model="newRecipe.name" type="text" id="name" required />
+      <label for="description">Description:</label>
       <input v-model="newRecipe.description" type="text" id="description" required />
 
       <label for="ingredients">Ingredients:</label>
       <input v-model="ingredientsInput" type="text" id="ingredients" required />
-      <p>Separate ingredients with commas (e.g., "Ingredient1, Ingredient2, Ingredient3")</p>
+      <p>Separate ingredients with commas <br> <br> (e.g., "Ingredient1, Ingredient2, Ingredient3")</p>
 
       <label for="instructions">Instructions:</label>
       <textarea v-model="newRecipe.instructions" id="instructions" required></textarea>
@@ -81,13 +82,23 @@ export default {
   margin: 0 auto;
 }
 
+#title {
+  border: 2px solid none;
+  border-radius: 15px;
+  background-color: rgb(255, 255, 255);
+  padding: 10px;
+  display: inline-block;
+}
+
 form {
   display: flex;
   flex-direction: column;
 }
 
 label {
-  margin-bottom: 5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
 input,
@@ -100,7 +111,7 @@ button {
   background-color: #4caf50;
   color: white;
   border: none;
-  border-radius: 4px;
+  border-radius: 15px;
   padding: 10px;
   cursor: pointer;
 }
